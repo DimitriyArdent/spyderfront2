dac9facd1cb0bfab12c3d7a1cc686cc4a496a8d2
DimitriyArdent <109689205+DimitriyArdent@users.noreply.github.com>
2023-04-08 01:46:43 +0300

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -2,16 +2,6 @@
 import axios from 'axios';
 
 
-export function advancedSearch(clickedURL, depth) {        // finds the hited URL
-    axios.get('http://localhost:8001/get-data', {
-        params: { targetLink: clickedURL, depth: depth }
-
-
-    }).then((res) => {
-        console.log(res)
-    })
-
-}
 
 
 export function targetWordSpotter(phrases, targetWord) {  // color the target word
@@ -27,3 +17,65 @@ export function targetWordSpotter(phrases, targetWord) {  // color the target wo
 }
 
 
+
+export function showTargetWordOccurences(key, label, phrases, setShowTargetWordModal, setModalPhrases) {
+    setModalPhrases(phrases)
+    setShowTargetWordModal(true)
+
+
+
+
+
+}
+
+export function createTreeObject(data) {
+
+    const obj = []
+
+    if (parseInt(data.value.Depth) === 1) {
+        let firstLevelObject = {}               // first level URL that appears on screen after initial search
+        firstLevelObject.key = data.selfKey
+        firstLevelObject.label = data.selfKey
+        firstLevelObject.phrases = JSON.parse(data.value.phrases)
+        firstLevelObject.nodes = []
+        for (let i = 0; i < JSON.parse(data.value.linksInThisUrl).length; i++) {
+            let node = {}
+            node.key = JSON.parse(data.value.linksInThisUrl)[i]
+            node.label = JSON.parse(data.value.linksInThisUrl)[i]
+
+            firstLevelObject.nodes.push(node)
+        }
+
+        obj.push(firstLevelObject)
+
+
+
+        return obj
+    }
+
+
+}
+
+export function createSecondObject(data, res) {
+    if (parseInt(data.value.Depth) === 2) {
+
+        //objects in array
+        for (let i = 0; i < res.length; i++) {
+            // nodes of this object
+            for (let j = 0; j < res[0].nodes.length; j++) {
+
+                if (res[i].nodes[j].key == data.selfKey) {
+                    res[i].nodes[j].nodes = []      // creating nodes field for this node(object in nodes array of his father)
+                    res[i].nodes[j].phrases = JSON.parse(data.value.phrases) // creating phrases field for this node(object in nodes array of his father)
+                    // url's of the data object
+                    for (let k = 0; k < JSON.parse(data.value.linksInThisUrl).length; k++) {
+                        res[i].nodes[j].nodes.push({ key: JSON.parse(data.value.linksInThisUrl)[k], label: JSON.parse(data.value.linksInThisUrl)[k] })
+                    }
+
+                }
+            }
+        }
+
+    }
+    return res
+}
